b4c4998b90c0342c2ad4b49b645a33664e6d8729
Armando Romero <arman2314@gmail.com>
2025-05-01 18:32:35 -0300

    ultimos cambios modulo 6 unidad 4

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import '../../styles/components/layout/Footer.css'
+const Footer = (props) => {
+    return(
+     <footer>
+        <p>Derechos reservados. Romero Armando Raul - 2025</p>
+     </footer>   
+    )
+}
+export default Footer;
---
+++ src/pages/NosotrosPage.js
@@ -0,0 +1,49 @@
+import '../styles/components/pages/NosotrosPage.css'
+const NosotrosPage = (props) => {
+    return (
+        <main className="holder">
+            <div className="historia">
+                <h2>Historia</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </div>
+            <div className="staff">
+                <h2>Staff</h2>
+                <div className="personas">
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros1.jpg" alt="Juan Gomez" />
+                        <h5>Juan Gomez</h5>
+                        <h6>Gerente General</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros2.jpg" alt="Diana Reyes" />
+                        <h5>Diana Reyes</h5>
+                        <h6>Gerente Comercial</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros3.jpg" alt="Roberto Zaptos" />
+                        <h5>Roberto Zaptos</h5>
+                        <h6>Gerente de Sistemas</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros4.jpg" alt="Sandra Mastropiero" />
+                        <h5>Sandra Mastropiero</h5>
+                        <h6>Gerente de Marketing</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className="persona">
+                        <img src="images/nosotros/nosotros5.jpg" alt="Luciano Figuero" />
+                        <h5>Luciano Figuero</h5>
+                        <h6>Gerente Logística</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+
+                </div>
+            </div>
+        </main>
+    )
+}
+export default NosotrosPage;
